4bbf022cac5b043f7d81c6bbf373455cc2aa9695
liilah <liisa.lahti@futurice.com>
2023-02-13 12:11:32 +0200

    Use env variables instead of secret.json

--- src/queries/nasa.ts
+++ src/queries/nasa.ts
@@ -1,11 +1,11 @@
 import { photosPerPage } from '@/constants'
 import { Photo, Rover } from '@/types'
-import secret from '../secret.json'
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
 export const getMetaData = async (rover: Rover) => {
-  const result = await fetch(
-    `${secret.baseUrl}/manifests/${rover}?api_key=${secret.apiKey}`
-  )
+  const result = await fetch(`${baseUrl}/manifests/${rover}?api_key=${apiKey}`)
   return result.json()
 }
 
@@ -15,7 +15,7 @@ export const getPhotos = async (
   sol: number
 ): Promise<{ photos: Photo[] }> => {
   const response: Response = await fetch(
-    `${secret.baseUrl}/rovers/${rover}/photos?sol=${sol}&page=${page}&api_key=${secret.apiKey}`
+    `${baseUrl}/rovers/${rover}/photos?sol=${sol}&page=${page}&api_key=${apiKey}`
   )
   const data = await response.json()
   return data
