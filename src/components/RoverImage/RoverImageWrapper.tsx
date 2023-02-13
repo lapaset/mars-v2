64077dbe646fbaeeb4a8b6a55b652befe5518d52
liilah <liisa.lahti@futurice.com>
2023-02-13 12:19:05 +0200

    Refactor

---
+++ src/components/RoverImage/RoverImageWrapper.tsx
@@ -0,0 +1,28 @@
+import { getMetaData } from '@/queries/nasa'
+import { Rover } from '@/types'
+import React, { FC } from 'react'
+import { useQuery } from 'react-query'
+import RoverImage from '.'
+import ImageContainer from '../ImageContainer'
+import LoadingIndicator from '../LoadingIndicator'
+
+type RoverImageWrapperProps = {
+  rover: Rover
+}
+
+const RoverImageWrapper: FC<RoverImageWrapperProps> = ({ rover }) => {
+  const meta = useQuery(['meta', rover], () => getMetaData(rover), { retry: 1 })
+  const maxSol = meta.status === 'success' && meta.data.photo_manifest.max_sol
+
+  return (
+    <ImageContainer>
+      {maxSol === undefined ? (
+        <LoadingIndicator />
+      ) : (
+        <RoverImage rover={rover} maxSol={maxSol} />
+      )}
+    </ImageContainer>
+  )
+}
+
+export default RoverImageWrapper
---
+++ src/components/RoverImage/index.tsx
@@ -0,0 +1,32 @@
+import { getPhotos } from '@/queries/nasa'
+import { Rover } from '@/types'
+import React, { FC } from 'react'
+import { useQuery } from 'react-query'
+import styled from 'styled-components'
+import LoadingIndicator from '../LoadingIndicator'
+
+type RoverImageProps = {
+  rover: Rover
+  maxSol: number
+}
+
+const RoverImage: FC<RoverImageProps> = ({ rover, maxSol }) => {
+  const photos = useQuery(['photos', rover, maxSol], () =>
+    getPhotos(1, rover, maxSol)
+  )
+
+  const latestPhoto =
+    photos.status === 'success' && photos?.data?.photos[0]?.img_src
+
+  return latestPhoto ? (
+    <Image src={latestPhoto} role="none" />
+  ) : (
+    <LoadingIndicator />
+  )
+}
+
+export default RoverImage
+
+const Image = styled.img`
+  width: 100%;
+`
