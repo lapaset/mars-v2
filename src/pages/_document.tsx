b638cce69b9b37e4d8f572f1b30c4d7e1c9d2b4a
liilah <liisa.lahti@futurice.com>
2023-02-13 12:10:00 +0200

    Update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Unbounded:wght@600&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
