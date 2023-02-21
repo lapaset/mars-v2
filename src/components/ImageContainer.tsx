84ce577ba688e30fea1e82ce22afde4736a1fe42
liilah <liisa.lahti@futurice.com>
2023-02-21 10:47:48 +0200

    Add hover style to frontpage photos

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -11,30 +11,58 @@ const ImageContainer: FC<ImageContainerProps> = ({ children }) => (
 
 const Container = styled.div`
   width: 100%;
-  min-height: 240px;
+  height: 100%;
   background: var(--image-background);
   display: flex;
   justify-content: center;
   align-items: center;
   border: 2px solid white;
   box-shadow: 0 -3px 10px white;
+  overflow: hidden;
 
   @media screen and (min-width: 600px) {
+    :hover {
+      box-sizing: content-box;
+      transition: margin 1s, width 1s, height 1s;
+      width: 105%;
+      height: 105%;
+    }
+
     :nth-child(1) {
-      box-shadow: -3px -3px 10px white;
+      box-shadow: -4px -4px 8px white;
+      :hover {
+        margin: -5% 0 0 -5%;
+        box-shadow: -4px -4px 20px white;
+      }
     }
 
     :nth-child(2) {
-      box-shadow: 3px -3px 10px white;
+      box-shadow: 4px -4px 8px white;
+      :hover {
+        margin: -5% 0 0;
+        box-shadow: 4px -4px 20px white;
+      }
     }
 
     :nth-child(3) {
-      box-shadow: -3px 3px 10px white;
+      box-shadow: -4px 4px 8px white;
+      :hover {
+        margin: 0 0 0 -5%;
+        box-shadow: -4px 4px 20px white;
+      }
     }
 
     :nth-child(4) {
-      box-shadow: 3px 3px 10px white;
+      box-shadow: 4px 4px 8px white;
+      :hover {
+        box-shadow: 4px 4px 20px white;
+      }
     }
+  }
+
+  @media screen and (min-width: 1024px) {
+    min-height: 300px;
+    min-width: 300px;
   }
 `
 
